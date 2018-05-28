ed1e195da42b59b0e2ad459d361d9bc050478c0f
Alek Hristov <alek.hristov@yahoo.com>
2018-05-29 01:45:53 +0300

    Courses View created

--- CoursesSystem/CoursesSystem.Web/Controllers/HomeController.cs
+++ CoursesSystem/CoursesSystem.Web/Controllers/HomeController.cs
@@ -5,11 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoursesSystem.Web.Models;
+using CoursesSystem.Services.Data.Contracts;
+using Bytes2you.Validation;
+using CoursesSystem.Utils.Contracts;
+using CoursesSystem.DTO;
 
 namespace CoursesSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICourseService courseService;
+        private readonly IMappingProvider mapper;
+
+        public HomeController(ICourseService courseService, IMappingProvider mapper)
+        {
+            Guard.WhenArgument(courseService, "Course Service can not be null!").IsNull().Throw();
+            Guard.WhenArgument(mapper, "Mapper can not be null!").IsNull().Throw();
+
+            this.courseService = courseService;
+            this.mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
@@ -39,9 +55,19 @@ namespace CoursesSystem.Web.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Courses()
+        public async Task<IActionResult> Courses()
         {
-            return View();
+            var model = new CoursesViewModel();
+
+            var availableCourses =  await this.courseService.GetAllAvailableCourses();
+            Guard.WhenArgument(availableCourses, "Available Courses can not be null!").IsNull().Throw();
+
+            var availableCoursesModel = mapper.ProjectTo<CourseDto, CourseViewModel>(availableCourses);
+            Guard.WhenArgument(availableCoursesModel, "Available Courses can not be null!").IsNull().Throw();
+
+            model.Courses = availableCoursesModel;
+
+            return View(model);
         }
     }
 }
