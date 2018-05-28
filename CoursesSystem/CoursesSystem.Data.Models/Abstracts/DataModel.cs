5b63bf3e44bddbc875f5fa516e498c0fa8d526b7
Alek Hristov <alek.hristov@yahoo.com>
2018-05-28 10:41:43 +0300

    Added Abstracts and Contracts folders

---
+++ CoursesSystem/CoursesSystem.Data.Models/Abstracts/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using CoursesSystem.Data.Models.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoursesSystem.Data.Models.Abstracts
+{
+    public abstract class DataModel : IAuditable, IDeletable
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
