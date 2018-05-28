47e58a0949913ef840dc314bc9303441c901a01e
Alek Hristov <alek.hristov@yahoo.com>
2018-05-28 12:15:23 +0300

    Implemented Ef Repository

---
+++ CoursesSystem/CoursesSystemDbContext/Repository/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using CoursesSystem.Data.Models.Contracts;
+using System.Linq;
+
+namespace CoursesSystem.Data.Repository.Contracts
+{
+    public interface IRepository<T> where T : class, IDeletable
+    {
+        IQueryable<T> All { get; }
+        IQueryable<T> AllAndDeleted { get; }
+
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
