e6909064314788b4dadc5421f34f81d0bf0b861b
Alek Hristov <alek.hristov@yahoo.com>
2018-05-28 12:41:36 +0300

    CourseDto created

---
+++ CoursesSystem/CoursesSystem.DTO/CourseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoursesSystem.DTO
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Credits { get; set; }
+
+        public string LecturerName { get; set; }
+    }
+}
