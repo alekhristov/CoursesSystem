848677e7fc73fc78edff5d4e07396c9d26707e5d
Alek Hristov <alek.hristov@yahoo.com>
2018-05-28 13:13:42 +0300

    Implemented IStudentService and ICourseService

---
+++ CoursesSystem/CoursesSystem.Services.Data/Contracts/ICourseService.cs
@@ -0,0 +1,22 @@
+﻿using CoursesSystem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoursesSystem.Services.Data.Contracts
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseDto>> GetAllAvailableCourses();
+
+        Task<bool> AddCourse(CourseDto course);
+
+        Task<bool> DeleteCourse(Guid courseId);
+
+        Task<bool> EditCourseName(Guid courseId, string courseNewName);
+
+        Task<bool> EditCourseCredits(Guid courseId, int courseNewCredits);
+
+        Task<bool> EditCourseLecturerName(Guid courseId, string courseNewLecturerName);
+    }
+}
---
+++ CoursesSystem/CoursesSystem.Services.Data/Contracts/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using CoursesSystem.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoursesSystem.Services.Data.Contracts
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<CourseDto>> GetAllRegisteredCourses(string studentId);
+
+        Task<IEnumerable<CourseDto>> GetAllNonRegisteredCourses(string studentId);
+    }
+}
