bf68f38e88596ae40989ccf13f03462be43f3ad1
Alek Hristov <alek.hristov@yahoo.com>
2018-05-29 13:34:19 +0300

    Separated Courses logic in a separate Controller

---
+++ CoursesSystem/CoursesSystem.Web/Controllers/CoursesController.cs
@@ -0,0 +1,40 @@
+﻿using Bytes2you.Validation;
+using CoursesSystem.DTO;
+using CoursesSystem.Services.Data.Contracts;
+using CoursesSystem.Utils.Contracts;
+using CoursesSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoursesSystem.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ICourseService courseService;
+        private readonly IMappingProvider mapper;
+
+        public CoursesController(ICourseService courseService, IMappingProvider mapper)
+        {
+            Guard.WhenArgument(courseService, "Course Service can not be null!").IsNull().Throw();
+            Guard.WhenArgument(mapper, "Mapper can not be null!").IsNull().Throw();
+
+            this.courseService = courseService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new CoursesViewModel();
+
+            var availableCourses = await this.courseService.GetAllAvailableCourses();
+            Guard.WhenArgument(availableCourses, "Available Courses can not be null!").IsNull().Throw();
+
+            var availableCoursesModel = mapper.ProjectTo<CourseDto, CourseViewModel>(availableCourses);
+            Guard.WhenArgument(availableCoursesModel, "Available Courses can not be null!").IsNull().Throw();
+
+            model.Courses = availableCoursesModel;
+
+            return View(model);
+        }
+    }
+}
--- CoursesSystem/CoursesSystem.Web/Controllers/HomeController.cs
+++ CoursesSystem/CoursesSystem.Web/Controllers/HomeController.cs
@@ -1,31 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CoursesSystem.Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using CoursesSystem.Web.Models;
-using CoursesSystem.Services.Data.Contracts;
-using Bytes2you.Validation;
-using CoursesSystem.Utils.Contracts;
-using CoursesSystem.DTO;
+using System.Diagnostics;
 
 namespace CoursesSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ICourseService courseService;
-        private readonly IMappingProvider mapper;
-
-        public HomeController(ICourseService courseService, IMappingProvider mapper)
-        {
-            Guard.WhenArgument(courseService, "Course Service can not be null!").IsNull().Throw();
-            Guard.WhenArgument(mapper, "Mapper can not be null!").IsNull().Throw();
-
-            this.courseService = courseService;
-            this.mapper = mapper;
-        }
-
         public IActionResult Index()
         {
             if (!User.Identity.IsAuthenticated)
@@ -33,7 +13,7 @@ namespace CoursesSystem.Web.Controllers
                 return this.View();
             }
 
-            return RedirectToAction("Courses");
+            return RedirectToAction("Index", "Courses");
         }
 
         public IActionResult About()
@@ -53,21 +33,6 @@ namespace CoursesSystem.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public async Task<IActionResult> Courses()
-        {
-            var model = new CoursesViewModel();
-
-            var availableCourses =  await this.courseService.GetAllAvailableCourses();
-            Guard.WhenArgument(availableCourses, "Available Courses can not be null!").IsNull().Throw();
-
-            var availableCoursesModel = mapper.ProjectTo<CourseDto, CourseViewModel>(availableCourses);
-            Guard.WhenArgument(availableCoursesModel, "Available Courses can not be null!").IsNull().Throw();
-
-            model.Courses = availableCoursesModel;
-
-            return View(model);
         }
     }
 }
