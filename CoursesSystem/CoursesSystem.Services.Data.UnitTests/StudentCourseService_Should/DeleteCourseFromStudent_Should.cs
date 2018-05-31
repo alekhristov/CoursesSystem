952afe1b22db5b4c75f5f3f6ab41a2698bad60fd
Alek Hristov <alek.hristov@yahoo.com>
2018-05-31 10:20:28 +0300

    StudentCourseService DeleteCourseFromStudent method tested

---
+++ CoursesSystem/CoursesSystem.Services.Data.UnitTests/StudentCourseService_Should/DeleteCourseFromStudent_Should.cs
@@ -0,0 +1,48 @@
+﻿using CoursesSystem.Data.Models;
+using CoursesSystem.Data.Repository.Contracts;
+using CoursesSystem.Data.Saver.Contracts;
+using CoursesSystem.Utils.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace CoursesSystem.Services.Data.UnitTests.StudentCourseService_Should
+{
+    [TestClass]
+    public class DeleteCourseFromStudent_Should
+    {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullStudentIdParameter()
+        {
+            //Arrange
+            var studentCoursesMock = new Mock<IRepository<StudentCourse>>();
+            var mapperMock = new Mock<IMappingProvider>();
+            var saverMock = new Mock<ISaver>();
+
+            var studentCourseService = new StudentCourseService(
+                studentCoursesMock.Object,
+                mapperMock.Object,
+                saverMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => studentCourseService.DeleteCourseFromStudent(Guid.NewGuid(), null));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentException_WhenInvokedWithInvalidEmptyStudentIdParameter()
+        {
+            //Arrange
+            var studentCoursesMock = new Mock<IRepository<StudentCourse>>();
+            var mapperMock = new Mock<IMappingProvider>();
+            var saverMock = new Mock<ISaver>();
+
+            var studentCourseService = new StudentCourseService(
+                studentCoursesMock.Object,
+                mapperMock.Object,
+                saverMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentException>(() => studentCourseService.DeleteCourseFromStudent(Guid.NewGuid(), string.Empty));
+        }
+    }
+}
